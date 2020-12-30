eb4b0bf7312a505728ef469d7d32cc93e35f94f0
kavitha <kavithasubbiah@hotmail.com>
2020-12-29 22:25:27 -0500

    Run with Spring adding AppConfig.java

---
+++ src/main/java/com/kav/springframework/AppConfig.java
@@ -0,0 +1,23 @@
+package com.kav.springframework;
+
+import com.kav.springframework.entities.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public Game game() {
+        return new BaseballGame(redSox(), cubs());
+    }
+
+    @Bean
+    public Team redSox() {
+        return new RedSox();
+    }
+
+    @Bean
+    public Team cubs(){
+        return new Cubs();
+    }
+}
--- src/main/java/com/kav/springframework/SpringframeworkApplication.java
+++ src/main/java/com/kav/springframework/SpringframeworkApplication.java
@@ -1,13 +1,22 @@
 package com.kav.springframework;
 
+import com.kav.springframework.entities.Game;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class SpringframeworkApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringframeworkApplication.class, args);
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Game game = context.getBean("game", Game.class);
+        System.out.println(game.playGame());
     }
 
 }
