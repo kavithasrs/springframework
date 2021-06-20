b82587b55174db876dd975f996f626373c993e40
kavitha <kavithasubbiah@hotmail.com>
2021-06-19 23:18:09 -0400

    Bean Count

--- src/main/java/com/kav/springframework/SpringframeworkApplication.java
+++ src/main/java/com/kav/springframework/SpringframeworkApplication.java
@@ -1,7 +1,11 @@
 package com.kav.springframework;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringframeworkApplication {
@@ -10,4 +14,18 @@ public class SpringframeworkApplication {
         SpringApplication.run(SpringframeworkApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(ApplicationContext ctx){
+        return args -> {
+            int beanCount = ctx.getBeanDefinitionCount();
+            System.out.println("There are " + beanCount + " beans in this application");
+            System.out.println("The bean names are: ");
+
+            for(String beanName : ctx.getBeanDefinitionNames()){
+                if(beanName.contains("AutoConfig")){
+                    System.out.println(beanName);
+                }
+            }
+        };
+    }
 }
